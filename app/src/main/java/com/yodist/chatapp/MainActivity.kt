8726d1c33eacbc5ec5487ad0539d5e4e53f239c8
Yodi Satriani <yodisatriani@live.com>
2022-11-09 01:16:29 +0700

    feat: Add token and api key to get chat list from stream
    MainActivity.kt was updated

--- app/src/main/java/com/yodist/chatapp/MainActivity.kt
+++ app/src/main/java/com/yodist/chatapp/MainActivity.kt
@@ -9,23 +9,73 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.yodist.chatapp.ui.theme.ChatAppTheme
+import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.logger.ChatLogLevel
+import io.getstream.chat.android.client.models.User
+import io.getstream.chat.android.compose.ui.channels.ChannelsScreen
+import io.getstream.chat.android.compose.ui.theme.ChatTheme
+import io.getstream.chat.android.offline.model.message.attachments.UploadAttachmentsNetworkType
+import io.getstream.chat.android.offline.plugin.configuration.Config
+import io.getstream.chat.android.offline.plugin.factory.StreamOfflinePluginFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 1 - Setup te OfflinePlugin for offline storage
+        val offlinePluginFactory = StreamOfflinePluginFactory(
+            config = Config(
+                backgroundSyncEnabled = true,
+                userPresence = true,
+                persistenceEnabled = true,
+                uploadAttachmentsNetworkType = UploadAttachmentsNetworkType.NOT_ROAMING,
+            ),
+            appContext = applicationContext,
+        )
+
+        // 2 - setup the client for API calls and with the plugin for offline storage
+        // get value from env variables
+        //val streamApiKey = System.getenv("STREAM_API_KEY")
+//        val client = ChatClient.Builder(streamApiKey!!, applicationContext)
+//            .withPlugin(offlinePluginFactory)
+//            .logLevel(ChatLogLevel.ALL)
+//            .build()
+        val client = ChatClient.Builder("b67pax5b2wdq", applicationContext)
+            .withPlugin(offlinePluginFactory)
+            .logLevel(ChatLogLevel.ALL)
+            .build()
+
+        // 3 - authenticate and connect the user
+        val user = User(
+            id = "tutorial-droid",
+            name = "Tutorial Droid",
+            image = "https://bit.ly/2TIt8NR"
+        )
+
+        val streamDummyToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidHV0b3JpYWwtZHJvaWQifQ.NhEr0hP9W9nwqV7ZkdShxvi02C5PR7SJE7Cs4y7kyqg"
+        client.connectUser(
+            user = user,
+            token = streamDummyToken
+        ).enqueue()
+
+        // 4 - Setup the Channels Screen UI
         setContent {
-            ChatAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            ChatTheme {
+                ChannelsScreen(
+                    title = stringResource(id = R.string.app_name),
+                    isShowingSearch = true,
+                    onItemClick = { channel ->
+                        // TODO start messages activity
+                    },
+                    onBackPressed = { finish() }
+                )
+
             }
         }
+
     }
 }
 
